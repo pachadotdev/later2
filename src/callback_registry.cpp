dcf22d54bf8cc1d11db4819bff2e8775b18e4d1c
M Vargas Sepulveda <m.vargas.sepulveda@gmail.com>
2026-05-12 18:34:09 +0100

    refactor callback registry

--- src/callback_registry.cpp
+++ src/callback_registry.cpp
@@ -12,55 +12,43 @@ static std::atomic<uint64_t> nextCallbackId(1);
 // StdFunctionCallback
 // ============================================================================
 
-StdFunctionCallback::StdFunctionCallback(Timestamp when, std::function<void(void)> func) :
-  Callback(when),
-  func(func)
-{
+StdFunctionCallback::StdFunctionCallback(Timestamp when,
+                                         std::function<void(void)> func)
+    : Callback(when), func(func) {
   this->callbackId = nextCallbackId++;
 }
 
-Rcpp::RObject StdFunctionCallback::rRepresentation() const {
-  using namespace Rcpp;
+cpp4r::sexp StdFunctionCallback::rRepresentation() const {
   ASSERT_MAIN_THREAD()
-
-  return List::create(
-    _["id"]       = callbackId,
-    _["when"]     = when.diff_secs(Timestamp()),
-    _["callback"] = Rcpp::CharacterVector::create("C/C++ function")
-  );
+  return cpp4r::writable::list(
+      {cpp4r::named_arg("id") = (double)callbackId,
+       cpp4r::named_arg("when") = when.diff_secs(Timestamp()),
+       cpp4r::named_arg("callback") = "C/C++ function"});
 }
 
-
 // ============================================================================
-// RcppFunctionCallback
+// cpp4rFunctionCallback
 // ============================================================================
 
-RcppFunctionCallback::RcppFunctionCallback(Timestamp when, const Rcpp::Function& func) :
-  Callback(when),
-  func(func)
-{
+cpp4rFunctionCallback::cpp4rFunctionCallback(Timestamp when, SEXP func)
+    : Callback(when), func(func) {
   ASSERT_MAIN_THREAD()
   this->callbackId = nextCallbackId++;
 }
 
-Rcpp::RObject RcppFunctionCallback::rRepresentation() const {
-  using namespace Rcpp;
+cpp4r::sexp cpp4rFunctionCallback::rRepresentation() const {
   ASSERT_MAIN_THREAD()
-
-  return List::create(
-    _["id"]       = callbackId,
-    _["when"]     = when.diff_secs(Timestamp()),
-    _["callback"] = func
-  );
+  return cpp4r::writable::list(
+      {cpp4r::named_arg("id") = (double)callbackId,
+       cpp4r::named_arg("when") = when.diff_secs(Timestamp()),
+       cpp4r::named_arg("callback") = static_cast<SEXP>(func)});
 }
-
 
 // ============================================================================
 // CallbackRegistry
 // ============================================================================
 
-// [[Rcpp::export(rng = false)]]
-void testCallbackOrdering() {
+[[cpp4r::register]] void testCallbackOrdering() {
   std::vector<StdFunctionCallback> callbacks;
   Timestamp ts;
   std::function<void(void)> func;
@@ -68,45 +56,41 @@ void testCallbackOrdering() {
     callbacks.push_back(StdFunctionCallback(ts, func));
   }
   for (size_t i = 1; i < 100; i++) {
-    if (callbacks[i] < callbacks[i-1]) {
-      ::Rcpp::stop("Callback ordering is broken [1]");
+    if (callbacks[i] < callbacks[i - 1]) {
+      cpp4r::stop("Callback ordering is broken [1]");
     }
-    if (!(callbacks[i] > callbacks[i-1])) {
-      ::Rcpp::stop("Callback ordering is broken [2]");
+    if (!(callbacks[i] > callbacks[i - 1])) {
+      cpp4r::stop("Callback ordering is broken [2]");
     }
-    if (callbacks[i-1] > callbacks[i]) {
-      ::Rcpp::stop("Callback ordering is broken [3]");
+    if (callbacks[i - 1] > callbacks[i]) {
+      cpp4r::stop("Callback ordering is broken [3]");
     }
-    if (!(callbacks[i-1] < callbacks[i])) {
-      ::Rcpp::stop("Callback ordering is broken [4]");
+    if (!(callbacks[i - 1] < callbacks[i])) {
+      cpp4r::stop("Callback ordering is broken [4]");
     }
   }
   for (size_t i = 100; i > 1; i--) {
-    if (callbacks[i-1] < callbacks[i-2]) {
-      ::Rcpp::stop("Callback ordering is broken [2]");
+    if (callbacks[i - 1] < callbacks[i - 2]) {
+      cpp4r::stop("Callback ordering is broken [2]");
     }
   }
 }
 
-CallbackRegistry::CallbackRegistry(int id, Mutex* mutex, ConditionVariable* condvar)
-  : id(id), mutex(mutex), condvar(condvar)
-{
+CallbackRegistry::CallbackRegistry(int id, Mutex *mutex,
+                                   ConditionVariable *condvar)
+    : id(id), mutex(mutex), condvar(condvar){ASSERT_MAIN_THREAD()}
+
+      CallbackRegistry::~CallbackRegistry() {
   ASSERT_MAIN_THREAD()
 }
 
-CallbackRegistry::~CallbackRegistry() {
-  ASSERT_MAIN_THREAD()
-}
+int CallbackRegistry::getId() const { return id; }
 
-int CallbackRegistry::getId() const {
-  return id;
-}
-
-uint64_t CallbackRegistry::add(const Rcpp::Function& func, double secs) {
-  // Copies of the Rcpp::Function should only be made on the main thread.
+uint64_t CallbackRegistry::add(SEXP func, double secs) {
+  // R functions should only be accessed from the main thread.
   ASSERT_MAIN_THREAD()
   Timestamp when(secs);
-  Callback_sp cb = std::make_shared<RcppFunctionCallback>(when, func);
+  Callback_sp cb = std::make_shared<cpp4rFunctionCallback>(when, func);
   Guard guard(mutex);
   queue.insert(cb);
   condvar->signal();
@@ -114,9 +98,10 @@ uint64_t CallbackRegistry::add(const Rcpp::Function& func, double secs) {
   return cb->getCallbackId();
 }
 
-uint64_t CallbackRegistry::add(void (*func)(void*), void* data, double secs) {
+uint64_t CallbackRegistry::add(void (*func)(void *), void *data, double secs) {
   Timestamp when(secs);
-  Callback_sp cb = std::make_shared<StdFunctionCallback>(when, std::bind(func, data));
+  Callback_sp cb =
+      std::make_shared<StdFunctionCallback>(when, std::bind(func, data));
   Guard guard(mutex);
   queue.insert(cb);
   condvar->signal();
@@ -145,17 +130,16 @@ Optional<Timestamp> CallbackRegistry::nextTimestamp(bool recursive) const {
 
   Optional<Timestamp> minTimestamp;
 
-  if (! this->queue.empty()) {
+  if (!this->queue.empty()) {
     cbSet::const_iterator it = queue.begin();
     minTimestamp = Optional<Timestamp>((*it)->when);
   }
 
   // Now check children
   if (recursive) {
-    for (std::vector<std::shared_ptr<CallbackRegistry> >::const_iterator it = children.begin();
-         it != children.end();
-         ++it)
-    {
+    for (std::vector<std::shared_ptr<CallbackRegistry>>::const_iterator it =
+             children.begin();
+         it != children.end(); ++it) {
       Optional<Timestamp> childNextTimestamp = (*it)->nextTimestamp(recursive);
 
       if (childNextTimestamp.has_value()) {
@@ -182,7 +166,7 @@ bool CallbackRegistry::empty() const {
 }
 
 // Returns true if the smallest timestamp exists and is not in the future.
-bool CallbackRegistry::due(const Timestamp& time, bool recursive) const {
+bool CallbackRegistry::due(const Timestamp &time, bool recursive) const {
   ASSERT_MAIN_THREAD()
   Guard guard(mutex);
   cbSet::const_iterator cbSet_it = queue.begin();
@@ -192,10 +176,9 @@ bool CallbackRegistry::due(const Timestamp& time, bool recursive) const {
 
   // Now check children
   if (recursive) {
-    for (std::vector<std::shared_ptr<CallbackRegistry> >::const_iterator it = children.begin();
-         it != children.end();
-         ++it)
-    {
+    for (std::vector<std::shared_ptr<CallbackRegistry>>::const_iterator it =
+             children.begin();
+         it != children.end(); ++it) {
       if ((*it)->due(time, true)) {
         return true;
       }
@@ -205,7 +188,7 @@ bool CallbackRegistry::due(const Timestamp& time, bool recursive) const {
   return false;
 }
 
-Callback_sp CallbackRegistry::pop(const Timestamp& time) {
+Callback_sp CallbackRegistry::pop(const Timestamp &time) {
   ASSERT_MAIN_THREAD()
   Guard guard(mutex);
   Callback_sp result;
@@ -242,32 +225,26 @@ bool CallbackRegistry::wait(double timeoutSecs, bool recursive) const {
       waitFor = 2;
     }
     condvar->timedwait(waitFor);
-    Rcpp::checkUserInterrupt();
+    cpp4r::check_user_interrupt();
   }
 
   return due();
 }
 
-
-Rcpp::List CallbackRegistry::list() const {
+cpp4r::sexp CallbackRegistry::list() const {
   ASSERT_MAIN_THREAD()
   Guard guard(mutex);
 
-  Rcpp::List results;
-
-  cbSet::const_iterator it;
-
-  for (it = queue.begin(); it != queue.end(); it++) {
-    results.push_back((*it)->rRepresentation());
+  cpp4r::sexp results(cpp4r::safe[Rf_allocVector](VECSXP, queue.size()));
+  size_t i = 0;
+  for (cbSet::const_iterator it = queue.begin(); it != queue.end(); ++it, ++i) {
+    cpp4r::sexp item = (*it)->rRepresentation();
+    SET_VECTOR_ELT(static_cast<SEXP>(results), i, static_cast<SEXP>(item));
   }
 
   return results;
 }
 
-void CallbackRegistry::fd_waits_incr() {
-  ++fd_waits;
-}
+void CallbackRegistry::fd_waits_incr() { ++fd_waits; }
 
-void CallbackRegistry::fd_waits_decr() {
-  --fd_waits;
-}
+void CallbackRegistry::fd_waits_decr() { --fd_waits; }
