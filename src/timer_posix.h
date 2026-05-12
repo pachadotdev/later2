3b59f284f9ccbfdd5f38ebbc10eade097f18aada
M Vargas Sepulveda <m.vargas.sepulveda@gmail.com>
2026-05-12 18:01:25 +0100

    clang format for timer_posix

--- src/timer_posix.h
+++ src/timer_posix.h
@@ -3,13 +3,13 @@
 
 #ifndef _WIN32
 
-#include <functional>
-#include "timestamp.h"
-#include "threadutils.h"
 #include "optional.h"
+#include "threadutils.h"
+#include "timestamp.h"
+#include <functional>
 
 class Timer {
-  std::function<void ()> callback;
+  std::function<void()> callback;
   Mutex mutex;
   ConditionVariable cond;
   // Stores the handle to a bgthread, which is created upon demand. (Previously
@@ -19,19 +19,19 @@ class Timer {
   Optional<Timestamp> wakeAt;
   bool stopped;
 
-  static int bg_main_func(void*);
+  static int bg_main_func(void *);
   void bg_main();
+
 public:
-  Timer(const std::function<void ()>& callback);
+  Timer(const std::function<void()> &callback);
   virtual ~Timer();
 
   // Schedules the timer to fire next at the specified time.
   // If the timer is currently scheduled to fire, that will
   // be overwritten with this one (the timer only tracks one
   // timestamp at a time).
-  void set(const Timestamp& timestamp);
+  void set(const Timestamp &timestamp);
 };
-
 
 #endif // _WIN32
 
