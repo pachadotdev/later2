c8b5c99c39c1e8ce4102cfec4506803b44e176f9
M Vargas Sepulveda <m.vargas.sepulveda@gmail.com>
2026-05-12 18:03:05 +0100

    clang format for interrupt.h

--- src/interrupt.h
+++ src/interrupt.h
@@ -9,17 +9,20 @@
 
 #endif // _WIN32
 
-
-// Borrowed from https://github.com/wch/r-source/blob/a0a6b159/src/include/R_ext/GraphicsDevice.h#L843-L858
+// Borrowed from
+// https://github.com/wch/r-source/blob/a0a6b159/src/include/R_ext/GraphicsDevice.h#L843-L858
 #ifndef BEGIN_SUSPEND_INTERRUPTS
 /* Macros for suspending interrupts */
-#define BEGIN_SUSPEND_INTERRUPTS do { \
-    Rboolean __oldsusp__ = R_interrupts_suspended; \
+#define BEGIN_SUSPEND_INTERRUPTS                                               \
+  do {                                                                         \
+    Rboolean __oldsusp__ = R_interrupts_suspended;                             \
     R_interrupts_suspended = TRUE;
-#define END_SUSPEND_INTERRUPTS R_interrupts_suspended = __oldsusp__; \
-    if (R_interrupts_pending && ! R_interrupts_suspended) \
-        Rf_onintr(); \
-} while(0)
+#define END_SUSPEND_INTERRUPTS                                                 \
+  R_interrupts_suspended = __oldsusp__;                                        \
+  if (R_interrupts_pending && !R_interrupts_suspended)                         \
+    Rf_onintr();                                                               \
+  }                                                                            \
+  while (0)
 
 #include <R_ext/libextern.h>
 LibExtern Rboolean R_interrupts_suspended;
@@ -27,6 +30,5 @@ LibExtern int R_interrupts_pending;
 extern void Rf_onintr(void);
 LibExtern Rboolean mbcslocale;
 #endif
-
 
 #endif
